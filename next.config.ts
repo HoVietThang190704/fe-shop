1c1bc8d2cb3a593f665bdaec5441e8a5fb8eeefe
Hồ Việt Thắng <154068613+HoVietThang190704@users.noreply.github.com>
2026-04-06 16:52:34 +0700

    fix nextconfig

--- next.config.ts
+++ next.config.ts
@@ -40,6 +40,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "loremflickr.com",
       },
+      {
+        protocol: "https",
+        hostname: "cdn.hstatic.net",
+      },
     ],
   },
   async rewrites() {
